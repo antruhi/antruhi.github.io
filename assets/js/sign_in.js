c7848b0acbaff1471d5bd3184dccd1d554f1a290
Kyoreo Man <kyoreoman@gmail.com>
2023-04-18 22:23:02 +0530

    updated chatbot

--- assets/js/sign_in.js
+++ assets/js/sign_in.js
@@ -1,10 +1,10 @@
-function chatPopup(){
-    console.log("## signed in - 02")
-    const myLandbotPopup = new LandbotPopup({
-      index: 'https://chats.landbot.io/v3/H-991902-B8YMZ7OY1RAF9PMQ/index.html',
-      open: true,
-      launcherOptions: {
-        enable: false,
-      }
-    });
-}
+// function chatPopup(){
+//     console.log("## signed in - 02")
+//     const myLandbotPopup = new LandbotPopup({
+//       index: 'https://chats.landbot.io/v3/H-991902-B8YMZ7OY1RAF9PMQ/index.html',
+//       open: true,
+//       launcherOptions: {
+//         enable: false,
+//       }
+//     });
+// }

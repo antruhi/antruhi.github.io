88a4a2ae576e8f3bc1e3a52869e42edcb2b83e50
Kyoreo Man <kyoreoman@gmail.com>
2023-04-19 00:58:02 +0530

    removed google signin

--- assets/js/sign_in.js
+++ assets/js/sign_in.js
@@ -2,15 +2,8 @@ function chatPopup(){
   window.addEventListener('mouseover', initLandbot, { once: true });
   window.addEventListener('touchstart', initLandbot, { once: true });
     console.log("## signed in - 02")
-    // const myLandbotPopup = new LandbotPopup({
-    //   index: 'https://chats.landbot.io/v3/H-991902-B8YMZ7OY1RAF9PMQ/index.html',
-    //   open: true,
-    //   launcherOptions: {
-    //     enable: false,
-    //   }
-    // });
-    var initLandbot = new Landbot.Popup({
-      configUrl: 'https://storage.googleapis.com/chats.landbot.io/v3/H-991902-B8YMZ7OY1RAF9PMQ/index.json',
+    const myLandbotPopup = new LandbotPopup({
+      index: 'https://chats.landbot.io/v3/H-991902-B8YMZ7OY1RAF9PMQ/index.html',
       open: true,
       launcherOptions: {
         enable: false,

99d5d50944674f4d32fa8b001d9e416c7bf23381
Kyoreo Man <kyoreoman@gmail.com>
2023-04-18 22:08:26 +0530

    added log

--- assets/js/sign_in.js
+++ assets/js/sign_in.js
@@ -1,4 +1,5 @@
 function chatPopup(){
+    console.log("## signed in - 02")
     const myLandbotPopup = new LandbotPopup({
       index: 'https://chats.landbot.io/v3/H-991902-B8YMZ7OY1RAF9PMQ/index.html',
       open: true,

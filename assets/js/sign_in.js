1ffe51b7213883ee22ba7e4eb83738d557c559aa
Kyoreo Man <kyoreoman@gmail.com>
2023-04-19 01:10:10 +0530

    chatpopup

--- assets/js/sign_in.js
+++ assets/js/sign_in.js
@@ -19,16 +19,9 @@ function chatPopup(){
   window.addEventListener('touchstart', initLandbot, { once: true });
   var myLandbot;
   function initLandbot() {
-    if (!myLandbot) {
-      var s = document.createElement('script');s.type = 'text/javascript';s.async = true;
-      s.addEventListener('load', function() {
-        myLandbot = new Landbot.Popup({
-          configUrl: 'https://storage.googleapis.com/chats.landbot.io/v3/H-991902-B8YMZ7OY1RAF9PMQ/index.json',
-        });
-      });
-      s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.js';
-      var x = document.getElementsByTagName('script')[0];
-      x.parentNode.insertBefore(s, x);
-    }
+    var myLandbot = new Landbot.Fullpage({
+      configUrl: 'https://storage.googleapis.com/chats.landbot.io/v3/H-991902-B8YMZ7OY1RAF9PMQ/index.json',
+    });
   }
 }
+
